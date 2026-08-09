97327866efdac437bbba8bbbd790897218bc7031
John Hodkinson <john_hodkinson@hotmail.co.uk>
2026-08-09 11:37:25 +0100

    fix: remove usages of prisma env import

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,11 @@
-import { env } from 'prisma/config'
-
 export const config = {
-	port: env('PORT') ? parseInt(env('PORT'), 10) : 3000,
-	browserSyncPort: env('BROWSER_SYNC_PORT'),
+	port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+	browserSyncPort: process.env.BROWSER_SYNC_PORT,
 	database: {
-		host: env('DATABASE_HOST'),
-		user: env('DATABASE_USER'),
-		password: env('DATABASE_PASSWORD'),
-		dbName: env('DATABASE_NAME'),
-		port: env('DATABASE_PORT'),
+		host: process.env.DATABASE_HOST,
+		user: process.env.DATABASE_USER,
+		password: process.env.DATABASE_PASSWORD,
+		dbName: process.env.DATABASE_NAME,
+		port: process.env.DATABASE_PORT,
 	},
 }
